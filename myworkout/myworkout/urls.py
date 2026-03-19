3b35b925142e4093eeaada30b4e78576636b5797
GaganMac <gagansd10@gmail.com>
2026-03-19 17:16:13 +0530

    home_url

--- myworkout/myworkout/urls.py
+++ myworkout/myworkout/urls.py
@@ -14,11 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('',include("workout.urls")),
+#     path("api/", include("workout.urls")),
+# ]
+
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path('',include("workout.urls")),
-    path("api/", include("workout.urls")),
+    path('api/', include("workout.urls")),
 ]
